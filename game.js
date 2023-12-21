fe53ca6d93e74cbffa128afa447ae79c9485c281
K4rol4 <karol.raciborski@gmail.com>
2023-12-21 19:45:34 +0100

    added playRound function

--- game.js
+++ game.js
@@ -3,3 +3,24 @@ function getComputerChoice() {
   const randomIndex = Math.floor(Math.random() * choices.length);
   return choices[randomIndex];
 }
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+
+  computerSelection = getComputerChoice().toLowerCase();
+
+  if (playerSelection === computerSelection) {
+    return "It's a tie";
+  }
+
+  if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    return `You Win! ${playerSelection} beats ${computerSelection}.`;
+  } else {
+    return `You Lose! ${computerSelection} beats ${playerSelection}.`;
+  }
+}
+
